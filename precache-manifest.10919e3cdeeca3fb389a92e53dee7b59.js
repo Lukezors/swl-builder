aa81608359f095561ca508a02800ce2cdd5e1a74
lchamberlain <lchamerlain@klipfolio.com>
2019-05-13 19:21:59 -0400

    Updates

---
+++ precache-manifest.10919e3cdeeca3fb389a92e53dee7b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774d296ea1f9c82b329f1d6c144bd929",
+    "url": "/swl-builder/index.html"
+  },
+  {
+    "revision": "7117057692ce49789a09",
+    "url": "/swl-builder/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/swl-builder/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "7117057692ce49789a09",
+    "url": "/swl-builder/static/js/main.165e4335.chunk.js"
+  },
+  {
+    "revision": "138cc62a8c3a614023f6",
+    "url": "/swl-builder/static/js/runtime~main.792c2997.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/swl-builder/static/media/logo.5d5d9eef.svg"
+  }
+]);
