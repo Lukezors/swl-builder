3255752ee91c6a869df34950d97cff9c2003ec0f
lchamberlain <lchamberlain@tripadvisor.com>
2019-11-20 15:15:26 -0500

    refactor constants and slots

--- src/components/UsageBar.js
+++ src/components/UsageBar.js
@@ -3,14 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import UsageBarItem from "./UsageBarItem";
-import {
-  RANK_COMMANDER,
-  RANK_OPERATIVE,
-  RANK_CORPS,
-  RANK_SPECIAL_FORCES,
-  RANK_HEAVY,
-  RANK_SUPPORT
-} from "../util/constants";
+import { RANKS } from "../util/constants";
 
 const UsageBarBody = styled.div`
   grid-column: 1 / span 2;
@@ -24,67 +17,69 @@ const UsageBarBody = styled.div`
 
 const UsageBar = ({
   points,
-  commanderSlots,
-  operativeSlots,
-  corpsSlots,
-  specialForcesSlots,
-  supportSlots,
-  heavySlots
-}) => (
+  slots
+}) => {
+    console.log(slots);
+    console.log(slots[RANKS.OPERATIVE]);
+    return (
   <UsageBarBody>
-    <UsageBarItem name={"Points"} currentValue={points} maxValue={800} />{" "}
+    <UsageBarItem name={"Points"} currentValue={points} maxValue={800} />
     {/*TODO: Points may need diff styling/ comp*/}
     <UsageBarItem
-      name={RANK_COMMANDER}
-      currentValue={commanderSlots}
+      name={RANKS.COMMANDER}
+      currentValue={slots[RANKS.COMMANDER]}
       maxValue={2}
     />
     <UsageBarItem
-      name={RANK_OPERATIVE}
-      currentValue={operativeSlots}
+      name={RANKS.OPERATIVE}
+      currentValue={slots[RANKS.OPERATIVE]}
       maxValue={2}
     />
     <UsageBarItem
-      name={RANK_SPECIAL_FORCES}
-      currentValue={corpsSlots}
+      name={RANKS.SPECIAL_FORCES}
+      currentValue={slots[RANKS.SPECIAL_FORCES]}
       maxValue={3}
     />
     <UsageBarItem
-      name={RANK_CORPS}
-      currentValue={specialForcesSlots}
+      name={RANKS.CORPS}
+      currentValue={slots[RANKS.CORPS]}
       maxValue={6}
     />
     <UsageBarItem
-      name={RANK_SUPPORT}
-      currentValue={supportSlots}
+      name={RANKS.SUPPORT}
+      currentValue={slots[RANKS.SUPPORT]}
       maxValue={3}
     />
-    <UsageBarItem
-        name={RANK_HEAVY}
-        currentValue={heavySlots}
-        maxValue={2}
+    <UsageBarItem 
+        name={RANKS.HEAVY} 
+        currentValue={slots[RANKS.HEAVY]} 
+        maxValue={2} 
     />
   </UsageBarBody>
-);
+);};
 
 UsageBar.propTypes = {
   points: PropTypes.number.isRequired,
-  commanderSlots: PropTypes.number.isRequired,
-  operativeSlots: PropTypes.number.isRequired,
-  corpsSlots: PropTypes.number.isRequired,
-  specialForcesSlots: PropTypes.number.isRequired,
-  supportSlots: PropTypes.number.isRequired,
-  heavySlots: PropTypes.number.isRequired
+  slots: PropTypes.shape({
+    [RANKS.COMMANDER]: PropTypes.number.isRequired,
+    [RANKS.OPERATIVE]: PropTypes.number.isRequired,
+    [RANKS.SPECIAL_FORCES]: PropTypes.number.isRequired,
+    [RANKS.CORPS]: PropTypes.number.isRequired,
+    [RANKS.SUPPORT]: PropTypes.number.isRequired,
+    [RANKS.HEAVY]: PropTypes.number.isRequired
+  })
 };
 
 UsageBar.defaultProps = {
   points: 0,
-  commanderSlots: 0,
-  operativeSlots: 0,
-  corpsSlots: 0,
-  specialForcesSlots: 0,
-  supportSlots: 0,
-  heavySlots: 0
+  slots: {
+    [RANKS.COMMANDER]: 0,
+    [RANKS.OPERATIVE]: 0,
+    [RANKS.SPECIAL_FORCES]: 0,
+    [RANKS.CORPS]: 0,
+    [RANKS.SUPPORT]: 0,
+    [RANKS.HEAVY]: 0
+  }
 };
 
 export default UsageBar;
--- src/util/constants.js
+++ src/util/constants.js
@@ -1,6 +1,9 @@
-export const RANK_COMMANDER = "Commander";
-export const RANK_OPERATIVE = "Operative";
-export const RANK_CORPS = "Corps";
-export const RANK_SPECIAL_FORCES = "Special Forces";
-export const RANK_SUPPORT = "Support";
-export const RANK_HEAVY = "Heavy";
+
+export const RANKS = {
+    COMMANDER: "Commander",
+    OPERATIVE :"Operative",
+    CORPS :"Corps",
+    SPECIAL_FORCES :"Special Forces",
+    SUPPORT :"Support",
+    HEAVY :"Heavy",
+};
